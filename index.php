a1dd052ad11551cddc81786cdfa89d4860b982df
DamianPacanowski <60273697+DamianPacanowski@users.noreply.github.com>
2022-05-15 19:16:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php	
-	require_once('config.php'); 
+	//** you can change to include_once('cookie.set.php'); or spl_autoload_register(function($_set){include_once($_set.".set.php");});
+	include_once('config.php');  
 	$_cookie_set = new cookie;
 	if(($_cookie_set->CookieSet())==true)
 	{
