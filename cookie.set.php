c768f72c2ebe6e3c7074fc48e9715a2dab83cc5f
DamianPacanowski <60273697+DamianPacanowski@users.noreply.github.com>
2022-05-17 14:12:10 +0200

    Update cookie.set.php

--- cookie.set.php
+++ cookie.set.php
@@ -1,19 +1,21 @@
 <?php
-	class cookie
+	class cookie // create a class
 	{
+		// set private variables $host, $ip, $programs, $this
 		private $host;	
 		private $ip;
 		private $programs;
 		private $this;
-		public function CookieSet() 
+		public function CookieSet() // set public function
 		{
+			// check variables existing
 			if(((empty($_SERVER['REMOTE_ADDR']))||($_SERVER['REMOTE_ADDR']=='')||($_SERVER['REMOTE_ADDR']==null))
 				||
 			((empty($_SERVER['HTTP_USER_AGENT']))||($_SERVER['HTTP_USER_AGENT']=='')||($_SERVER['HTTP_USER_AGENT']==null))
 				||
 			((empty(gethostbyaddr($_SERVER['REMOTE_ADDR'])))||(gethostbyaddr($_SERVER['REMOTE_ADDR'])=='')||(gethostbyaddr($_SERVER['REMOTE_ADDR'])==null)))	
 			{
-				return false;
+				return false; // variables not exsist
 			}
 			elseif(((!empty($_SERVER['REMOTE_ADDR']))||($_SERVER['REMOTE_ADDR']!='')||($_SERVER['REMOTE_ADDR']!=null))
 				||
@@ -21,33 +23,35 @@
 				||
 			((!empty(gethostbyaddr($_SERVER['REMOTE_ADDR'])))||(gethostbyaddr($_SERVER['REMOTE_ADDR'])!='')||(gethostbyaddr($_SERVER['REMOTE_ADDR'])!=null)))
 			{
-				if(is_numeric(str_replace('.','',$_SERVER['REMOTE_ADDR'])))
+				// variables exsist
+				if(is_numeric(str_replace('.','',$_SERVER['REMOTE_ADDR']))) // verife ip variable
 				{
+					// set variables
 					$host=str_replace('=','',base64_encode(gethostbyaddr($_SERVER['REMOTE_ADDR'])));
 					$ip=str_replace('=','',base64_encode($_SERVER['REMOTE_ADDR']));
 					$programs=str_replace('=','',base64_encode(str_replace(' ','',$_SERVER['HTTP_USER_AGENT'])));
 					$this->host=$host;
 					$this->ip=$ip;
 					$this->programs=$programs;					
-					if((self::SetCookie())&&(self::SaveCookie()))
+					if((self::SetCookie())&&(self::SaveCookie())) // execute private finctions
 					{
-						return true;
+						return true; // execute successfully
 					} 
 					else 
 					{
-						return false; 
+						return false; // execute false 
 					}
 				}
 			}
 		}
-		private function SetCookie() 
+		private function SetCookie() // create private funktion
 		{
 			setcookie(str_replace('=','',base64_encode('host')),$this->host,0,'/',str_replace('www.','',$_SERVER['SERVER_NAME']),1,1);
 			setcookie(str_replace('=','',base64_encode('ip')),$this->ip,0,'/',str_replace('www.','',$_SERVER['SERVER_NAME']),1,1);
 			setcookie(str_replace('=','',base64_encode('programs')),$this->programs,0,'/',str_replace('www.','',$_SERVER['SERVER_NAME']),1,1);
 			return true;
 		}
-		private function SaveCookie() 
+		private function SaveCookie() // create private funktion
 		{
 			fopen('../cookie/hosts/'.$this->host,'w');
 			fopen('../cookie/ips/'.$this->ip,'w');
