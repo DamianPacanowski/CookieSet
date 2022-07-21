222733ae2585d8eae932a360cd6810b818f6ad9f
DamianPacanowski <60273697+DamianPacanowski@users.noreply.github.com>
2022-07-21 23:15:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php	
-	// you can change \include_once('config.php');/
-	// to \include_once('cookie.set.php');/
-	// or \spl_autoload_register(function($set){include_once($set.".set.php");});/
 	include('cookie.set.php'); // class file
 	$cookie_set = new cookie; // class call
 	if(($cookie_set->CookieSet())==true) // class function execution true
